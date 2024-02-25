af4911d8ecb686e89079544e7eeab43f2dfca412
Josselynv <verajosselyn13@gmail.com>
2024-02-24 21:07:17 -0500

    Reportes actualizados

--- src/components/generarReporte.jsx
+++ src/components/generarReporte.jsx
@@ -1,67 +1,103 @@
-import React, { useEffect } from "react";
-import jsPDF from "jspdf";
-import "jspdf-autotable";
+import React, { useEffect } from 'react';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 const GenerarReportePDF = ({ empresaInfo, logoEmpresa, empleadosData }) => {
-  // Función para generar el reporte PDF
-  const generarReportePDF = () => {
-    // Crear un nuevo documento PDF
-    const doc = new jsPDF();
 
-    // Agregar información de la empresa al PDF
-    doc.setFontSize(12);
-    doc.text(`Nombre de la empresa: ${empresaInfo.enombre}`, 10, 10);
-    doc.text(`Dirección: ${empresaInfo.direccion}`, 10, 20);
-    doc.text(`Teléfono: ${empresaInfo.etelefono}`, 10, 30);
+    // Función para generar el reporte PDF
+    const generarReportePDF = () => {
+        // Crear un nuevo documento PDF
+        const doc = new jsPDF();
 
-    // Agregar logo de la empresa al PDF si está disponible
-    if (logoEmpresa) {
-      const logo = new Image();
-      logo.src = logoEmpresa;
-      doc.addImage(logo, "PNG", 150, 5, 40, 40);
-    }
+        // Definir coordenadas y dimensiones de las franjas
+        const topRectY = 0; // Coordenada Y de la franja superior
+        const bottomRectY = doc.internal.pageSize.getHeight() - 20; // Coordenada Y de la franja inferior
+        const rectWidth = doc.internal.pageSize.getWidth(); // Ancho de la franja es igual al ancho de la página
+        const rectHeight = 30; // Altura de las franjas
+        const rectHeighty = 20; // Altura de las franjas
 
-    // Agregar título y encabezados de la tabla de empleados
-    doc.text("Reporte de Empleados", 10, 50);
-    const headers = [["Nombre", "Apellido", "Teléfono", "Ciudad", "Fecha"]];
+        doc.setFillColor(194, 18, 18); // Rojo: RGB(255, 0, 0)
 
-    // Extraer datos de los empleados para la tabla
-    const data = empleadosData.map((empleado) => [
-      empleado.nombre,
-      empleado.apellido,
-      empleado.telefono,
-      empleado.ciudad,
-      empleado.fecha,
-    ]);
+        // Dibujar franja superior
+        doc.rect(0, topRectY, rectWidth, rectHeight, 'F');
 
-    // Agregar la tabla de empleados al PDF
-    doc.autoTable({
-      startY: 60,
-      head: headers,
-      body: data,
-    });
+        // Dibujar franja inferior
+        doc.rect(0, bottomRectY, rectWidth, rectHeighty, 'F');
 
-    // Obtener la fecha actual
-    const fechaEmision = new Date().toLocaleDateString();
+        // Agregar logo de la empresa al PDF si está disponible
+        if (logoEmpresa) {
+            
+            const logo = new Image();
+            logo.src = logoEmpresa;
+            const logoWidth = 30; // Ancho del logo
+            const logoX = 5; // Coordenada X del logo
+            const logoY = 0; // Coordenada Y del logo
+            doc.addImage(logo, 'PNG', logoX, logoY, logoWidth, logoWidth); // Colocar el logo en el lado izquierdo
+        }
+        
+        doc.setTextColor(255, 255, 255); // Blanco: RGB(255, 255, 255)
+        doc.setFontSize(15);
+        doc.text(`${empresaInfo.enombre}`, 32, 12); // Información de la empresa al lado del logo
+        doc.setFontSize(12);
+        doc.text(`${empresaInfo.direccion}`, 32, 17);
+        doc.setFontSize(11);
+        doc.text(`${empresaInfo.etelefono}`, 32, 22);
+        doc.setFontSize(12);
+        doc.setTextColor(0, 0, 0); 
 
-    // Agregar la fecha de emisión al PDF
-    const fechaHoraEmision = new Date().toLocaleString();
+        // Agregar título y encabezados de la tabla de empleados
+        doc.text('Reporte de Empleados', 10, 40);
+        
+        const headers = [['Nombre', 'Apellido', 'Teléfono', 'Ciudad', 'Fecha']];
 
-    // Agregar la fecha y hora de emisión al PDF
-    doc.text(
-      `Fecha y hora de emisión: ${fechaHoraEmision}`,
-      10,
-      doc.autoTable.previous.finalY + 10
-    );
+        // Extraer datos de los empleados para la tabla
+        const data = empleadosData.map(empleado => [
+            empleado.nombre,
+            empleado.apellido,
+            empleado.telefono,
+            empleado.ciudad,
+            empleado.fecha,
+        ]);
 
-    // Guardar el documento PDF
-    doc.save("reporte_empleados.pdf");
-  };
+        // Agregar la tabla de empleados al PDF
+        doc.autoTable({
+            startY: 48,
+            head: headers,
+            body: data,
+        });
 
-  // Llamamos a la función para generar el reporte PDF
-  generarReportePDF();
+        // Obtener la fecha actual
+        const fechaEmision = new Date().toLocaleDateString();
 
-  return null; // No necesitamos renderizar ningún elemento visible en el DOM
+        // Agregar la fecha de emisión al PDF
+        const fechaHoraEmision = new Date().toLocaleString();
+
+        // Agregar la fecha y hora de emisión al PDF
+        // Obtener el ancho de la página
+        const pageWidth = doc.internal.pageSize.getWidth();
+
+        // Obtener el ancho del texto
+        const fontSize = 10;
+        const fechaTextWidth = doc.getStringUnitWidth(`Fecha y hora de emisión: ${fechaHoraEmision}`) * fontSize / doc.internal.scaleFactor;
+
+        // Calcular la posición del texto
+        const xPosition = pageWidth - fechaTextWidth - 10; // 10 es el margen derecho
+        const yPosition = doc.internal.pageSize.getHeight() - 10; // 10 es el margen inferior
+
+        // Establecer el tamaño de fuente y renderizar el texto
+        doc.setFontSize(fontSize);
+        doc.setTextColor(255, 255, 255);
+        doc.text(`Fecha y hora de emisión: ${fechaHoraEmision}`, xPosition, yPosition);
+
+
+        // Guardar el documento PDF
+        doc.save('reporte_empleados.pdf');
+    };
+
+    // Llamamos a la función para generar el reporte PDF
+    generarReportePDF();
+
+    return null; // No necesitamos renderizar ningún elemento visible en el DOM
 };
 
 export default GenerarReportePDF;
