505d6ba2a5c8c322d2f8882a505dcb7f3d1bee13
Josselynv <verajosselyn13@gmail.com>
2024-03-24 17:35:54 -0500

    agg vista sucursal pag inicio

--- src/Clientes/sucursal.jsx
+++ src/Clientes/sucursal.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Card, Row, Col } from 'antd';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import API_URL from '../config';
 import imgsucur from './res/logoempresa.png';
 import fondo from './res/backgroungl.png';
+import { Link } from 'react-router-dom'; // Asegúrate de tener instalado react-router-dom
 
 const Sucursales = () => {
     const [sucursales, setSucursales] = useState([]);
     const [selectedSucursal, setSelectedSucursal] = useState(null);
+    const mapRef = useRef(null);
 
     useEffect(() => {
         const fetchSucursales = async () => {
@@ -23,6 +25,14 @@ const Sucursales = () => {
         fetchSucursales();
     }, []);
 
+    useEffect(() => {
+        if (selectedSucursal && mapRef.current) {
+            mapRef.current.flyTo([selectedSucursal.latitud, selectedSucursal.longitud], 13, {
+                duration: 1
+            });
+        }
+    }, [selectedSucursal]);
+
     const handleVerUbicacion = (latitud, longitud) => {
         setSelectedSucursal({ latitud, longitud });
     };
@@ -32,6 +42,7 @@ const Sucursales = () => {
             <div style={{ backgroundColor: '#BF0000', padding: '15px', display: 'flex', alignItems: 'center' }}>
                 <img src={imgsucur} alt="Logo de la empresa" style={{ height: '50px', marginRight: '10px' }} />
                 <h3 style={{ color: 'white', margin: 0, fontSize: '22px' }}>Hamburguesas al carbón</h3>
+                <Link to="http://localhost:5173" style={{ color: 'white', marginLeft: '1080px' }}>Inicio</Link>
             </div>
             <div style={{ padding: '20px' }}>
                 <h2 style={{ textAlign: 'center', marginBottom: '20px', color: 'black', fontSize: '23px' }}>Nuestras Sucursales</h2>
@@ -46,7 +57,7 @@ const Sucursales = () => {
                                         <img
                                             alt={sucursal.snombre}
                                             src={`data:image/png;base64, ${sucursal.imagensucursal}`}
-                                            style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+                                            style={{ width: '100%', height: '250px', objectFit: 'cover' }}
                                         />
                                     )
                                 }
@@ -59,7 +70,7 @@ const Sucursales = () => {
                 </Row>
             </div>
             {selectedSucursal && (
-                <MapContainer center={[selectedSucursal.latitud, selectedSucursal.longitud]} zoom={13} style={{ height: '400px', width: '100%' }}>
+                <MapContainer ref={mapRef} center={[selectedSucursal.latitud, selectedSucursal.longitud]} zoom={13} style={{ height: '400px', width: '100%' }}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
